2ddf8b77fc3a9d422aee651d10c3c3adf14dc7e0
pauzed <pauzed@gmail.com>
2013-05-01 21:39:32 +0000

    rake db command

--- db/migrate/install_spree_mail.rb
+++ db/migrate/install_spree_mail.rb
@@ -1,25 +1,21 @@
 class InstallSpreeMail < ActiveRecord::Migration
-  
-  def self.up
-    create_table :subscribers do |t|
+  def change
+    create_table :spree_subscribers do |t|
       t.string     :token
       t.string     :name
       t.string     :email
       t.datetime   :unsubscribed_at
       t.timestamps
     end
-    create_table :emails do |t|
+
+    create_table :spree_emails do |t|
       t.string     :token
       t.text       :to
       t.string     :subject
       t.text       :body
       t.timestamps
     end
-  end
 
-  def self.down
-    drop_table :subscribers
-    drop_table :emails
   end
 
 end
